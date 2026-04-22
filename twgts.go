1e13849e327418358ebc0f2c596c1d6e3b8ad2a9
Juan Carlos Gutierrez <jgutierrez@contractor.ea.com>
2026-04-22 11:07:35 -0700

    fix: Fix on sync tasks

--- twgts.go
+++ twgts.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"gitlab.com/jcgutier/jcgutier/Golang/taskSyncPOC/config"
 	taskssync "gitlab.com/jcgutier/jcgutier/Golang/taskSyncPOC/tasksSync"
@@ -10,20 +11,20 @@ import (
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	for true {
-	cfg, err := config.LoadConfig()
-	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
-	}
+	for {
+		cfg, err := config.LoadConfig()
+		if err != nil {
+			log.Fatalf("Failed to load config: %v", err)
+		}
 
-	taskSync, err := taskssync.NewTasksSync(cfg)
-	if err != nil {
-		log.Fatalf("Failed to create tasks sync: %v", err)
+		taskSync, err := taskssync.NewTasksSync(cfg)
+		if err != nil {
+			log.Fatalf("Failed to create tasks sync: %v", err)
+		}
+		err = taskSync.Sync()
+		if err != nil {
+			log.Fatalf("Failed to sync tasks: %v", err)
+		}
+		time.Sleep(20 * time.Second)
 	}
-	err = taskSync.Sync()
-	if err != nil {
-		log.Fatalf("Failed to sync tasks: %v", err)
-	}
-	
-}
 }
